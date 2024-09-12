87207107c48d01dce490a8f27c7492af28c0c615
IagorBM <iagorbremmmalaguti@gmail.com>
2024-09-12 16:43:07 -0300

    quero perder todo o progresso não

---
+++ Apache24/htdocs/teste1.php
@@ -0,0 +1,17 @@
+<?php
+    print("Quem disse que Falmengo é time?<br>");//os dois funcionam da mesma maneira
+    echo"Flamengo não é time não<br>";// mas utiliza echo pq não precisa das ()
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+
+        </title>
+    </head>
+
+    <body>
+        time é o vaxco da gama
+    </body>
+</html>
